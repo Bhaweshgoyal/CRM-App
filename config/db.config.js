c3022e8e6fd47355c729a604491081acb26f686a
Bhaweshgoyal <105268824+Bhaweshgoyal@users.noreply.github.com>
2023-04-22 22:26:14 +0530

    Add files via upload

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
 const mongoDbUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/crmApp';
-const dbName = "crmapp-db";
+// const mongoDbUri = "mongodb+srv://goyalb567:nUyHmgdJ3mDfgFpw@crmapp-db.fftgwip.mongodb.net/?retryWrites=true&w=majority"
+const dbName = "Cluster0";
 module.exports = { mongoDbUri, dbName };
