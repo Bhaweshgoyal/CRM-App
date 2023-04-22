358df08466d2f196210374863149a55437273f45
Bhaweshgoyal <105268824+Bhaweshgoyal@users.noreply.github.com>
2023-04-22 20:46:43 +0530

    db configed changed

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,5 @@
-const mongoURI = "mongodb://127.0.0.1:27017/CRM_App";
+require('dotenv').config();
 
-
-module.exports = {mongoURI}
+const mongoDbUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/crmApp';
+const dbName = "crmapp-db";
+module.exports = { mongoDbUri, dbName };
